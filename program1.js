b32b310435939c1a1d9e49876a622c43282206c4
muskan rajoriya <144930686+mussu-2360@users.noreply.github.com>
2024-09-27 12:44:02 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,8 +1,55 @@
-const getTotalIsles = function (grid) {
+function countIslands(grid) {
+    if (!grid || grid.length === 0) return 0;
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let islandCount = 0;
+
+    
+    function dfs(i, j) {
+        
+        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === 'W') {
+            return;
+        }
+
+        
+        grid[i][j] = 'W';
+
+       
+        dfs(i + 1, j); 
+        dfs(i - 1, j); 
+        dfs(i, j + 1); 
+        dfs(i, j - 1); 
+    }
+
+    
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            
+            if (grid[i][j] === 'L') {
+                islandCount++;
+                dfs(i, j); 
+            }
+        }
+    }
+
+    return islandCount;
+}
 
 
-  // write your code here
+const map1 = [
+    ["L", "L", "L", "L", "W"],
+    ["L", "L", "W", "L", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+];
+console.log(countIslands(map1)); 
 
-};
 
-module.exports = getTotalIsles;
+const map2 = [
+    ["L", "L", "W", "W", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "L", "W", "W"],
+    ["W", "W", "W", "L", "L"],
+];
+console.log(countIslands(map2)); 
